fe5571c0f565eb55452c736439ed63cd0aa1ae03
Serhii Marushchenko <serhii.mar90@gmail.com>
2018-12-28 19:52:07 +0200

    was added public method into budgetController which let create the new elements

--- app/js/common.js
+++ app/js/common.js
@@ -4,9 +4,55 @@
 var budgetController = (function () {
     'use strict';
 
-    // some code
+    // Constructor
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
 
+    // Constructor
+    var Expenses = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
 
+    var budgetData = {
+        allItems: {
+            plus: [],
+            minus: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
+
+    return {
+        publicAddItem: function (action, description, value) {
+            var newItem, ID;
+            // ID must be on 1 num bigger than last item into array. On example [1,2,3,4] - ID = 5, or [1,3,5] - ID = 6
+            if (budgetData.allItems[action].length > 0) {
+                ID = budgetData.allItems[action][budgetData.allItems[action].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (action === 'plus') {
+                newItem = new Income(ID, description, value);
+            } else {
+                newItem = new Expenses(ID, description, value);
+            }
+
+            budgetData.allItems[action].push(newItem);
+
+            return newItem;
+        },
+        publicShow: function () {
+            console.log(budgetData);
+        }
+    };
 
 })();
 
@@ -28,37 +74,30 @@ var uIController = (function () {
     var _nodes = {
         selectAction: document.getElementById(_nodeClass.nodeId.selectAction), // will geting plus or minus
         inputDescription: document.getElementById(_nodeClass.nodeId.inputDescription),
-        inputValue: document.getElementById(_nodeClass.nodeId.inputValue)  
+        inputValue: document.getElementById(_nodeClass.nodeId.inputValue)
     };
 
-    // function return array
-    function _getNodesValue(nodes) {
-        var values = []
-        for( var node in nodes) {
-            values.push(nodes[node].value);
-        }
-        return values;
-        // return {
-        //     selectAction: document.getElementById(_nodeClass.nodeId.selectAction).value, // will geting plus or minus
-        //     inputDescription: document.getElementById(_nodeClass.nodeId.inputDescription).value,
-        //     inputValue: document.getElementById(_nodeClass.nodeId.inputValue).value    
-        // };
+    // function returns {}
+    function _getNodesValue(items) {
+        var Nodes = items;
+
+        return {
+            selectAction: Nodes.selectAction.value, // will geting plus or minus
+            inputDescription: Nodes.inputDescription.value,
+            inputValue: Nodes.inputValue.value
+        };
     }
 
     function _setNode(name, node) {
         _nodes[name] = node;
     }
 
-
     return {
-        publicGetValue: function() {
-            return _getNodesValue(_nodes); // return array
+        publicGetValue: function () {
+            return _getNodesValue(_nodes); // return {}
         },
-        publicGetNodeClass: function() {
-            return _nodeClass; // return object
-        },
-        pulicGetNodes: function() {
-            return _nodes; // return object
+        publicGetNodeClass: function () {
+            return _nodeClass; // return {}
         }
     };
 
@@ -69,8 +108,11 @@ var controller = (function (budgetConstr, UIConstr) {
     'use strict';
 
     function workWithData() {
+        var nodeValues, newItem;
         //1.Get the fiald input data;
-        var nodeValues = UIConstr.publicGetValue(); // array
+        nodeValues = UIConstr.publicGetValue(); // {}
+        // 2.Send getting fiald data to budget controller;
+        newItem = budgetConstr.publicAddItem(nodeValues.selectAction, nodeValues.inputDescription, parseInt(nodeValues.inputValue));
         /*
             1.Get the fiald input data;
             2.Send getting fiald data to budget controller;
@@ -78,8 +120,9 @@ var controller = (function (budgetConstr, UIConstr) {
             4.Calculate budget;
             5.Update budget UI in UI Controller;
         */
-    //    console.log(nodeValues);
-       return nodeValues;
+        //    console.log(nodeValues);
+        console.log(newItem);
+        // return nodeValues;
     }
 
     function setUpEventListeners() {
@@ -87,15 +130,15 @@ var controller = (function (budgetConstr, UIConstr) {
 
         document.getElementsByClassName(DOM.nodeClass.btnSend)[0].addEventListener('click', workWithData);
 
-        document.addEventListener('keydown', function(event) {
-    
+        document.addEventListener('keydown', function (event) {
+
             if (event.key === 'Enter') workWithData();
-            
-        });    
+
+        });
     }
 
     return {
-        init: function() {
+        init: function () {
             console.log('Aplication is started.');
             return setUpEventListeners();
         }
